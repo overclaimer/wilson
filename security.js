265939216b94defcfb84d4d9c62762c31292287f
overclaimer <overclaimer@gmail.com>
2021-07-09 08:57:27 -0400

    Ignore security.js
    That's not allowed for the public.

---
+++ security.js
@@ -0,0 +1,7 @@
+var data = {
+    server: `841078509170327553`,
+    wilson: `863032017934614569`,
+    token: `ODYzMDMyMDE3OTM0NjE0NTY5.YOg_Mw.gzXuWlGseSXFlvr-s-UHT730AzA`
+};
+
+module.exports = { data };
