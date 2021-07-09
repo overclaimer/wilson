b7cf15e485cf7bec3e113b4bd8dc94f9e95c8adb
overclaimer <overclaimer@gmail.com>
2021-07-09 08:56:44 -0400

    added basic bot logic

--- index.js
+++ index.js
@@ -1,1 +1,38 @@
-console.log("lol");
+const Discord = require(`discord.js`);
+const client = new Discord.Client();
+
+const { data } = require('./security.js');
+const nickname = `Wilson`;
+
+
+client.on(`ready`, () => {
+    log(`Client ready.`);
+    change_nickname();
+});
+
+client.on(`interaction`, async interaction => {
+	if (!interaction.isCommand()) return;
+	if (interaction.commandName === `ping`) {
+		await interaction.reply(`Pong!`);
+	}
+});
+
+var change_nickname = () =>{
+    log(`Changing nickname..`);
+    var server = get_main_server();
+    server.members.cache.forEach(member => {
+        if(member.id == `${data.wilson}`){
+            member.setNickname(`${nickname}`);
+            log(`Changed nickname to ${nickname}!`);
+        }
+    }); 
+}
+var get_main_server = () =>{
+    log(`Getting main discord server..`);
+    return client.guilds.cache.get(`${data.server}`);
+}
+var log = (v) =>{
+    console.log(`[Wilson] ${v}`);
+}
+
+client.login(`${data.token}`);
