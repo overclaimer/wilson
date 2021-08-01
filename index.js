b4c65922594e3cefbc1f739660aa119881276e79
overclaimer <overclaimer@gmail.com>
2021-08-01 16:25:01 -0400

    Adding news
    Update before system reset.

--- index.js
+++ index.js
@@ -1,16 +1,20 @@
 const Discord = require(`discord.js`);
 const client = new Discord.Client();
+const NewsAPI = require('newsapi');
+const fs = require(`fs`);
 
 const { data } = require('./security.js');
 const nickname = `Wilson`;
 
 
-client.on(`ready`, () => {
+client.on(`ready`, async () => {
     log(`Client ready.`);
     change_nickname();
+
+    await GetTopNews();
 });
 
-client.on('message', message => {
+client.on('message', async (message) => {
 
     if(message.author.bot){
         return; // Do not reply to bots. (Or self.)
@@ -22,15 +26,26 @@ client.on('message', message => {
     if(c[0].toLowerCase() == `wilson`){
         if(c[1] != null){
             if(c[1].toLowerCase() == `help`){
-                message.reply(`You cannot be helped.`);
+                var link = `https://github.com/overclaimer/wilson/wiki/`;
+                message.channel.send(`You can view all my info here: ${link}`);
+            }
+            if(c[1].toLowerCase() == `code`){
+                var link = `https://github.com/overclaimer/wilson`;
+                message.channel.send(`You can view my code on github: ${link}`);
             }
         }else{
-            message.reply(`You can view commands with *wilson help*`);
+            message.channel.send(`You can view commands with *wilson help*`);
         }
     }else{
-        var r = random(0,5);
+        var r = random(0,50);
         if(r == 1){
-            message.reply(`Interesting.`);
+            message.channel.send(`Interesting.`);
+        }
+        if(r == 2){
+            message.channel.send(`I agree!`);
+        }
+        if(r == 3){
+            message.channel.send(`Say wat..`);
         }
     }
 });
@@ -57,4 +72,25 @@ client.login(`${data.token}`);
 
 var random = (min, max)=>{
     return Math.floor((Math.random() * max) + min);
+}
+
+var GetTopNews = async ()=>{
+    log(`Getting news..`);
+    var story = ``;
+    const newsapi = new NewsAPI(data.newsApi);
+    var a = await newsapi.v2.topHeadlines({
+        language: 'en'
+    }).then(response => {
+        return response;
+    });
+    var ttt = a.totalResults;
+    var sss = random(0, ttt);
+    var l = 0;
+    a.articles.forEach((art)=>{
+        if(l == sss){
+            console.log(art);
+        }
+        l++;
+    });
+    
 }
